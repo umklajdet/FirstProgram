3b8e5cf7a5dd000cce7d4f906fa31eb5875fab4b
umklajdet <pfedorov85@gmail.com>
2021-04-08 23:53:34 +0300

    добавлено решение Задачи №3

--- src/Tasks.java
+++ src/Tasks.java
@@ -8,5 +8,18 @@ public class Tasks {
         double q = 131.45;
         byte w = 127;
         System.out.println("q = " + q + "\nw = " + w);
+
+        /* задача 3
+        Задайте массив из 5 любых целых чисел.
+        Поменяйте местами первый и последний элемент в массиве.
+        Вывести в консоль результат суммы первого и среднего элемента.
+        */
+        int[] arrayTask3 = new int[5];
+        arrayTask3 = new int[]{10, 78, 65, 33, 784};
+        int change = arrayTask3[0];
+        arrayTask3[0] = arrayTask3[4];
+        arrayTask3[4] = change;
+        int sum = arrayTask3[0] + arrayTask3[2];
+        System.out.println("Сумма первого и среднего элемента массива = " + sum);
     }
 }
